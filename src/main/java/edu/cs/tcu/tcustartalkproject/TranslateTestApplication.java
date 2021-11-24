e88767786842d071b12ae5be4bfcbfe793e3fc0e
James Fanning <j.fanning@tcu.edu>
2021-11-23 20:51:31 -0600

    Added dependencies for Google translate, added 2 classes TranslateController and TranslateTestApplication for implementing translate.

---
+++ src/main/java/edu/cs/tcu/tcustartalkproject/TranslateTestApplication.java
@@ -0,0 +1,10 @@
+package edu.cs.tcu.tcustartalkproject;
+import edu.cs.tcu.tcustartalkproject.utils.TranslateController;
+
+public class TranslateTestApplication {
+
+    public static void main(String... args) throws Exception {
+        TranslateController.translateText();
+
+    }
+}
