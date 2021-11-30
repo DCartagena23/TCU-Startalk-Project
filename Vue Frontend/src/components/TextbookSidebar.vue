bfdd4b5fa7f46cffd56406c9be65aa7c1edf9ea3
rluly <ryanluly1@gmail.com>
2021-11-29 20:01:12 -0600

    Sidebar edits

--- Vue Frontend/src/components/TextbookSidebar.vue
+++ Vue Frontend/src/components/TextbookSidebar.vue
@@ -4,7 +4,7 @@
   <div class="card">
     <div class="card-header" id="headingOne">
       <h5 class="mb-0">
-        <button id="ch1" class="btn btn section" v-on:click="toggleCollapse" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <button id="ch1" class="btn btn section1" v-on:click="toggleCollapse" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           Chapter 1
         </button>
       </h5>
@@ -14,12 +14,18 @@
       <div class="card-body section">
         Section 1
       </div>
+      <div class="card-body section">
+        Section 2
+      </div>
+      <div class="card-body section">
+        Section 3
+      </div>
     </div>
   </div>
   <div class="card">
     <div class="card-header" id="headingTwo">
       <h5 class="mb-0">
-        <button id="ch2" class="btn btn collapsed section" v-on:click="toggleCollapse2" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <button id="ch2" class="btn btn collapsed section1" v-on:click="toggleCollapse2" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
           Chapter 2
         </button>
       </h5>
@@ -28,12 +34,18 @@
       <div class="card-body section">
         Section 1
       </div>
+      <div class="card-body section">
+        Section 2
+      </div>
+      <div class="card-body section">
+        Section 3
+      </div>
     </div>
   </div>
   <div class="card">
     <div class="card-header" id="headingThree">
       <h5 class="mb-0">
-        <button id="ch3" class="btn btn collapsed section" v-on:click="toggleCollapse3" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <button id="ch3" class="btn btn collapsed section1" v-on:click="toggleCollapse3" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
           Chapter 3
         </button>
       </h5>
@@ -41,6 +53,12 @@
     <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
       <div class="card-body section">
         Section 1
+      </div>
+      <div class="card-body section">
+        Section 2
+      </div>
+      <div class="card-body section">
+        Section 3
       </div>
     </div>
   </div>
@@ -53,6 +71,9 @@
 .section:hover{
     color: blue;
     cursor: pointer;
+}
+.section1:hover{
+  cursor: pointer;
 }
 </style>
 
