76362c500d6d469983eb4c7b8d3688c76610e622
Quang Truong <251minhquang@gmail.com>
2022-02-14 15:30:43 -0600

    fix bug regarding

--- src/main/java/edu/cs/tcu/tcustartalkproject/TcuStartalkProjectApplication.java
+++ src/main/java/edu/cs/tcu/tcustartalkproject/TcuStartalkProjectApplication.java
@@ -17,18 +17,18 @@ import java.util.Scanner;
 @SpringBootApplication
 public class TcuStartalkProjectApplication{
     public static void main(String[] args) throws FileNotFoundException {
-//        Scanner sc = new Scanner(new File("src/main/resources/rootkey.csv"));
-//        sc.useDelimiter("=|\\n");   //sets the delimiter pattern
-//        String[] accessKey = new String[4];
-//        for (int i = 0; sc.hasNext() != false; i++){
-//            accessKey[i] = sc.next();
-//        }
-//        sc.close();  //closes the scanner
-//        AWSCredentials credentials = new BasicAWSCredentials(
-//                accessKey[1],
-//                accessKey[3]
-//        );
-//        AmazonS3 s3client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_1).build();
+        Scanner sc = new Scanner(new File("src/main/resources/rootkey.csv"));
+        sc.useDelimiter("=|\\n");   //sets the delimiter pattern
+        String[] accessKey = new String[4];
+        for (int i = 0; sc.hasNext() != false; i++){
+            accessKey[i] = sc.next();
+        }
+        sc.close();  //closes the scanner
+        AWSCredentials credentials = new BasicAWSCredentials(
+                accessKey[1],
+                accessKey[3]
+        );
+        AmazonS3 s3client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_1).build();
         SpringApplication.run(TcuStartalkProjectApplication.class, args);
     }
 }
